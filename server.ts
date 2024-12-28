63e563d25ded6c94c35380a8e2e1d58ed78f9dcb
Sajid Ahmad <sajid01ahmad@gmail.com>
2024-12-28 23:59:24 +0530

    add a config for env variables

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import app from "./src/app";
+import { config } from "./src/config/config";
 
 const startServer = () => {
-  const port = process.env.PORT || 3000;
+  const port = config.port || 3000;
 
   app.listen(port, () => {
     console.log(`server is listening on port ${port}`);

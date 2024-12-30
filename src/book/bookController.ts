2dd82b6a4437828981d173321243c5d7caaa7cd5
Sajid Ahmad <sajid01ahmad@gmail.com>
2024-12-31 00:28:07 +0530

    cloudinary configuration

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 
-const createBook = (req, res, next: NextFunction) => {};
+const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  // const {}=req.body;
+  console.log("files",req.files)
+  res.json({});
+};
 
 export { createBook };
--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,9 @@ const _config = {
   database_Url: process.env.DATABASE_URL,
   env: process.env.NODE_ENV,
   jwtSecret: process.env.JWT_SECRET,
+  cloudinary_cloud: process.env.CLOUDINARY_CLOUD,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
 };
 
 export const config = Object.freeze(_config);

3111d1212ea8d0fbfc509dd8e872be1029fb9507
Sajid Ahmad <sajid01ahmad@gmail.com>
2025-01-01 00:02:39 +0530

    removed the unwanted var in globalError Handler

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,13 +1,8 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { HttpError } from "http-errors";
 import { config } from "../config/config";
 
-const globalErrorHandler = (
-  err: HttpError,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const globalErrorHandler = (err: HttpError, req: Request, res: Response) => {
   const statusCode = err.statusCode || 500;
   res.status(statusCode).json({
     message: err.message,
